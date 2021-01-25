4273368928f0635e05765d8ae0e4c5dfe9092801
Boyan Vasilev <vasilev1878@gmail.com>
2021-01-25 11:47:13 -0600

    Implement printJob

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -62,6 +62,7 @@ public class TechJobs {
 
                 if (searchField.equals("all")) {
                     System.out.println("Search all fields not yet implemented.");
+
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -111,6 +112,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //System.out.println("printJobs is not implemented yet");
+
+        if (someJobs.size() <= 0) {
+            System.out.println("No jobs found.");
+        } else {
+            for (HashMap<String, String> aJob : someJobs) {
+                System.out.println("*****");
+                for (String jobKey : aJob.keySet()) {
+                    System.out.println(jobKey + " : " + aJob.get(jobKey));
+                }
+            }
+            System.out.println("*****");
+        }
     }
 }
